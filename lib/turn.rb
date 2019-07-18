50916779961340c37300734f8ae6d210e01eb8c1
Zoey Fryhover <znfryhover@gmail.com>
2019-07-18 19:28:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index)
+  if index.between?(0, 8) && !(position_taken?(board, index))
     return true
   else
     return false
